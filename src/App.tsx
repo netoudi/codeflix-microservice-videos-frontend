972648194552710455fb833e71bb2a336f335452
netoudi <netoudi@rocketmail.com>
2020-01-13 15:30:54 -0300

    Estilizando componente Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,6 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
-import { Delete } from '@material-ui/icons';
-import logo from './logo.svg';
-import './App.css';
+import { Navbar } from './components/Navbar';
 
-const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <Button color="secondary">
-        <Delete color="secondary" />
-        Delete
-      </Button>
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const App: React.FC = () => <Navbar />;
 
 export default App;
