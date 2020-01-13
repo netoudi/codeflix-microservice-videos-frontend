613109b2f9b8838ae317655172a9bca2ade2f87f
netoudi <netoudi@rocketmail.com>
2020-01-13 16:27:31 -0300

    Criando menu no Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,11 +2,16 @@ import * as React from 'react';
 import {
   AppBar,
   Button,
+  IconButton,
   makeStyles,
+  Menu,
+  MenuItem,
   Theme,
   Toolbar,
   Typography,
 } from '@material-ui/core';
+
+import MenuIcon from '@material-ui/icons/Menu';
 
 import logo from '../../static/img/logo.png';
 
@@ -29,12 +34,41 @@ const useStyles = makeStyles((theme: Theme) => ({
 export const Navbar: React.FC = () => {
   const classes = useStyles();
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleOpen = (event: any) => setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
+
   return (
     <AppBar>
       <Toolbar className={classes.toolbar}>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          aria-controls="menu-appbar"
+          aira-haspopup="true"
+          onClick={handleOpen}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        <Menu
+          id="menu-appbar"
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+          transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+          getContentAnchorEl={null}
+        >
+          <MenuItem onClick={handleClose}>Categories</MenuItem>
+        </Menu>
+
         <Typography className={classes.title}>
           <img src={logo} alt="CodeFlix" className={classes.logo} />
         </Typography>
+
         <Button color="inherit">Login</Button>
       </Toolbar>
     </AppBar>
