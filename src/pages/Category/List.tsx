a07136c92d7cde43dcbb3642496b747e82501889
netoudi <netoudi@rocketmail.com>
2020-01-14 14:19:18 -0300

    Criando Componente De Listagem De Categorias

--- src/pages/Category/List.tsx
+++ src/pages/Category/List.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Fab } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
 import { Page } from '../../components/Page';
+import Table from './Table';
 
 type ListProps = {};
 
 const List: React.FC = (props: ListProps) => (
-  <Page title="Listar categorias">Listar categorias</Page>
+  <Page title="Listagem categorias">
+    <Box dir="rtl">
+      <Fab
+        title="Adicionar categoria"
+        size="small"
+        component={Link}
+        to="/categories/create"
+      >
+        <AddIcon />
+      </Fab>
+    </Box>
+    <Box>
+      <Table />
+    </Box>
+  </Page>
 );
 
 export default List;
---
+++ src/pages/Category/Table.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+type TableProps = {};
+
+const Table: React.FC = (props: TableProps) => {
+  return <h1>Table</h1>;
+};
+
+export default Table;
