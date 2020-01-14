57fcaeaa318a2bca5166e58059e2d90b578b80a5
netoudi <netoudi@rocketmail.com>
2020-01-14 08:35:21 -0300

    Definindo mapa de rotas

---
+++ src/pages/Category/List.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Page } from '../../components/Page';
+
+type ListProps = {};
+
+const List: React.FC = (props: ListProps) => (
+  <Page title="Listar categorias">Listar categorias</Page>
+);
+
+export default List;
