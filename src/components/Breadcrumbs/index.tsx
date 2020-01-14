94f4e837aaa83afe3e8a6c3bc454415830e3a1fe
netoudi <netoudi@rocketmail.com>
2020-01-14 11:47:45 -0300

    Montando breadcrumb

---
+++ src/components/Breadcrumbs/index.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import Link, { LinkProps } from '@material-ui/core/Link';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import MUIBreadcrumbs from '@material-ui/core/Breadcrumbs';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { Route } from 'react-router';
+import { Link as RouterLink } from 'react-router-dom';
+import { Location } from 'history';
+import RouteParser from 'route-parser';
+import routes from '../../routes';
+
+const breadcrumbNameMap: { [key: string]: string } = {};
+routes.forEach((route) => {
+  breadcrumbNameMap[route.path as string] = route.label;
+});
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    linkRouter: {
+      color: '#4db5ab',
+      '&:focus, &:active': {
+        color: '#4db5ab',
+        textDecoration: 'none',
+      },
+      '&:hover': {
+        color: '#055a52',
+        textDecoration: 'none',
+      },
+    },
+  }),
+);
+
+interface LinkRouterProps extends LinkProps {
+  to: string;
+  replace?: boolean;
+}
+
+const LinkRouter = (props: LinkRouterProps) => (
+  <Link {...props} component={RouterLink as any} />
+);
+
+export default function Breadcrumbs() {
+  const classes = useStyles();
+
+  function makeBreadcrumbs(location: Location) {
+    const pathnames = location.pathname.split('/').filter((x) => x);
+    pathnames.unshift('/'); // ['/', 'categories', 'create']
+
+    return (
+      <MUIBreadcrumbs aria-label="breadcrumb">
+        {pathnames.map((value, index) => {
+          const last = index === pathnames.length - 1;
+          const to = pathnames
+            .slice(0, index + 1)
+            .join('/')
+            .replace('//', '/'); // /categories | /categories
+          const route = Object.keys(breadcrumbNameMap).find((path) =>
+            new RouteParser(path).match(to),
+          );
+
+          if (route === undefined) return false;
+
+          return last ? (
+            <Typography color="textPrimary" key={to}>
+              {breadcrumbNameMap[route]}
+            </Typography>
+          ) : (
+            <LinkRouter
+              color="inherit"
+              to={to}
+              key={to}
+              className={classes.linkRouter}
+            >
+              {breadcrumbNameMap[route]}
+            </LinkRouter>
+          );
+        })}
+      </MUIBreadcrumbs>
+    );
+  }
+
+  return (
+    <Container>
+      <Route>{({ location }) => makeBreadcrumbs(location)}</Route>
+    </Container>
+  );
+}
