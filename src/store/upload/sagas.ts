7fc7e3768d3d28266696ffbb52d4ff21d5d9a861
netoudi <netoudi@rocketmail.com>
2020-05-25 08:55:00 -0300

    Capturando novos arquivos de upload com Saga

--- src/store/upload/sagas.ts
+++ src/store/upload/sagas.ts
@@ -1,1 +1,12 @@
-export function* uploadWatcherSaga() {}
+import { actionChannel, take } from 'redux-saga/effects';
+import { Types } from './index';
+
+export function* uploadWatcherSaga() {
+  const newFilesChannel = yield actionChannel(Types.ADD_UPLOAD);
+
+  while (true) {
+    const { payload } = yield take(newFilesChannel);
+
+    console.log(payload);
+  }
+}
